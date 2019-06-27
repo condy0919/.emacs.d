e4844fa833abf532ca26e801291042f8aac78a92
Zhiwei Chen <chenzhiwei.condy@bytedance.com>
2019-06-27 18:35:30 +0800

    使用 projectile 方便查找项目下文件

--- plugins/init-git.el
+++ plugins/init-git.el
@@ -2,5 +2,15 @@
   :ensure t
   :bind (("C-x g" . magit-status)))
 
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode t)
+  (setq projectile-completion-system 'ivy)
+  :bind (("C-c p a" . projectile-find-file)))
+
+(use-package treemacs-projectile
+  :ensure t)
+
 
 (provide 'init-git)
