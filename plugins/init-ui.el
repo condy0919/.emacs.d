5bf4634a099d0fbb495f19f171439155d6b9313e
condy <condy0919@gmail.com>
2019-12-04 18:55:14 +0800

    windows 间跳转可用 evil-mode 来完成

--- plugins/init-ui.el
+++ plugins/init-ui.el
@@ -8,9 +8,6 @@
 (use-package posframe
   :ensure t)
 
-(use-package zenburn-theme
-  :ensure t)
-
 (use-package doom-themes
   :ensure t
   :config
@@ -19,19 +16,6 @@
 (use-package doom-modeline
   :ensure t
   :hook (after-init . doom-modeline-mode))
-
-;; windows 间跳转
-(use-package ace-window
-  :ensure t
-  :init
-  (progn
-    (setq aw-scope 'global) ;; was frame
-    (global-set-key (kbd "C-x O") 'other-frame)
-    (global-set-key [remap other-window] 'ace-window)
-    (custom-set-faces
-     '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-    ))
 
 ;; ivy 依赖
 (use-package counsel
