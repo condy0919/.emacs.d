3368a920dfb3ffcfad398499a068d0ae8aed4ae2
condy <condy0919@gmail.com>
2019-12-14 07:51:07 +0800

    Revert "使用空格，拒绝 tab"
    This reverts commit c9673ba279bfc3a945230bfdaf4153998060e28c.

--- plugins/init-base.el
+++ plugins/init-base.el
@@ -6,10 +6,7 @@
       blink-cursor-mode nil
       scroll-conservatively 1000)
 
-;; never use tabs
-(setq-default indent-tabs-mode nil
-              c-basic-offset 4
-              tab-width 4)
+(setq-default indent-tabs-mode nil)
 
 (set-face-attribute 'default nil :height 140)
 
