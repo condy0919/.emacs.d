677d15bff827588d53d3735f3829a55fa24cca6c
condy <condy0919@gmail.com>
2020-01-02 00:10:49 +0800

    lsp-ui apply action 绑定至 C-c C-a

--- plugins/init-lsp.el
+++ plugins/init-lsp.el
@@ -47,14 +47,25 @@
   :ensure t
   :commands lsp-ui-mode
   :hook (lsp-mode . lsp-ui-mode)
+  :bind (:map lsp-ui-mode-map
+         ("C-c C-a" . lsp-ui-sideline-apply-code-actions))
   :config
-  (setq lsp-ui-doc-max-height 10
-        lsp-ui-doc-max-width 40
-        lsp-ui-doc-header nil
-        lsp-ui-doc-include-signature nil
-        lsp-ui-sideline-show-hover 1
-        lsp-ui-sideline-ignore-duplicate t
-        lsp-ui-flycheck-enable t)
+  (setq
+    ;; doc
+    lsp-ui-doc-max-height 30
+    lsp-ui-doc-max-width 60
+    lsp-ui-doc-header nil
+    lsp-ui-doc-include-signature nil
+    ;; sideline
+    lsp-ui-sideline-enable t
+    lsp-ui-sideline-ignore-duplicate t
+    lsp-ui-sideline-show-hover t
+    lsp-ui-sideline-show-symbol t
+    lsp-ui-sideline-show-diagnostics t
+    lsp-ui-sideline-show-code-actions t
+    lsp-ui-sideline-code-action-prefix ""
+    ;; flycheck
+    lsp-ui-flycheck-enable t)
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
 
