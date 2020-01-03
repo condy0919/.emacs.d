81194fd8c0c35ba145f9851b88b2ebf9f4d70ed3
condy <condy0919@gmail.com>
2020-01-04 04:05:34 +0800

    ace-window: reconfig

--- plugins/init-tools.el
+++ plugins/init-tools.el
@@ -59,8 +59,31 @@
   :bind (:map global-map
          ("M-o" . ace-window))
   :config
+  (set-face-attribute
+   'aw-leading-char-face nil
+   :foreground "deep sky blue"
+   :weight 'bold
+   :height 3.0)
+  (set-face-attribute
+   'aw-mode-line-face nil
+   :inherit 'mode-line-buffer-id
+   :foreground "lawn green")
   (setq aw-keys '(?a ?s ?d ?f ?h ?j ?k ?l)
-        aw-scope 'frame))
+        aw-scope 'frame
+        aw-dispatch-always t
+        aw-dispatch-alist '((?x aw-delete-window "Ace - Delete Window")
+                            (?c aw-swap-window "Ace - Swap Window")
+                            (?n aw-flip-window)
+                            (?v aw-split-window-vert "Ace - Split Vert Window")
+                            (?h aw-split-window-horz "Ace - Split Horz Window")
+                            (?m delete-other-windows "Ace - Maximize Window")
+                            (?g delete-other-windows)
+                            (?b balance-windows)
+                            (?u (lambda ()
+                                  (progn
+                                    (winner-undo)
+                                    (setq this-command 'winner-undo))))
+                            (?r winner-redo))))
 
 (provide 'init-tools)
 
