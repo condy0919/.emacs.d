160f0be65925fb8886bec6477dbda240a4f3a2b2
condy <condy0919@gmail.com>
2020-01-25 12:06:27 +0800

    Speedup uptime

--- early-init.el
+++ early-init.el
@@ -5,5 +5,12 @@
 
 (setq package-enable-at-startup nil)
 
+;; Faster to disable these here (before they've been initialized)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(when (featurep 'ns)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
+
 (provide 'early-init)
 ;;; early-init.el ends here
