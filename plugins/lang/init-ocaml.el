9916a1c057b36f09223833d649135dcc6130a0f4
condy <condy0919@gmail.com>
2020-02-08 14:19:07 +0800

    reasonml

--- plugins/lang/init-ocaml.el
+++ plugins/lang/init-ocaml.el
@@ -5,16 +5,24 @@
 
 ;;; Code:
 
+;; ocaml mode
 (use-package tuareg
   :ensure t)
 
+;; Context sensitive completion
 (use-package merlin
   :ensure t
   :hook (tuareg-mode . merlin-mode)
   :custom
   (merlin-error-after-save nil))
 
+;; dune build system
 (use-package dune
+  :ensure t)
+
+;; DONT forget to install https://github.com/reasonml/reason-cli
+;; ocaml dialect for web
+(use-package reason-mode
   :ensure t)
 
 (provide 'init-ocaml)
