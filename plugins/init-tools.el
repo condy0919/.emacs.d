e929a2f059e3726d51144107b396fed67df12204
condy <condy0919@gmail.com>
2020-03-07 19:04:21 +0800

    Revert "Recenter after jump"
    This reverts commit b1a80dbb8ed356a3469aaa0f993292cf36264244.

--- plugins/init-tools.el
+++ plugins/init-tools.el
@@ -59,10 +59,6 @@
 (use-package counsel
   :ensure t
   :diminish counsel-mode
-  :custom
-  (counsel-grep-post-action-hook '(recenter))
-  (counsel-find-file-at-point t)
-  (counsel-find-file-ignore-regexp "\\(?:\\`\\(?:\\.\\|__\\)\\|elc\\|pyc$\\)")
   :hook (ivy-mode . counsel-mode)
   :bind (("M-y" . counsel-yank-pop)
          ("M-x" . counsel-M-x)
@@ -78,11 +74,9 @@
    '(("d" delete-file "delete")
      ("r" rename-file "rename")
      ("x" counsel-find-file-as-root "open as root")))
-
-  ;; recenter after jump
-  (advice-add #'counsel-imenu :after (lambda (&rest _)
-                                       (recenter)))
-  )
+  :custom
+  (counsel-find-file-at-point t)
+  (counsel-find-file-ignore-regexp "\\(?:\\`\\(?:\\.\\|__\\)\\|elc\\|pyc$\\)"))
 
 ;; DONT use swiper
 (use-package isearch
