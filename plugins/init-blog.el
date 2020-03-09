3914872b89d0f2c6c49f54061a4bc301e74366bf
condy <condy0919@gmail.com>
2020-03-10 02:19:35 +0800

    Blog facility

--- plugins/init-blog.el
+++ plugins/init-blog.el
@@ -2,19 +2,65 @@
 
 ;;; Commentary:
 ;;
+;; Modified from
+;; https://github.com/ksqsf/emacs-config/blob/master/modules/prelude-blog.el
 
 ;;; Code:
 
-(require 'markdown-mode)
+(defgroup mblog nil
+  "Quick manipulation of blog posts."
+  :group 'text)
 
-(defun my/markdown-insert-ruby-tag ()
-  "Insert ruby tag."
+(defcustom mblog-posts-directory (expand-file-name "~/blog/_posts")
+  "The directory for your blog posts."
+  :group 'mblog
+  :type '(string))
+
+(defun mblog--generate-header (title)
+  "Customize `TITLE' and generate a header."
+  (format "---
+layout: post
+tilte: %s
+categories: note
+usemermaid: false
+usemathjax: false
+---
+
+* content
+{:toc}
+" title))
+
+(defun mblog-insert-ruby-tag (text ruby)
+  "Quick insertion of ruby tag with `TEXT' and `RUBY'."
+  (interactive "sText: \nsRuby: \n")
+  (insert (format "<ruby>%s<rp>(</rp><rt>%s</rt><rp>)</rp></ruby>"
+                  text
+                  ruby)))
+
+;;;###autoload
+(defun mblog-new-post (title permalink)
+  "Create a new blog post with `TITLE' which can be visited by `PERMALINK'."
+  (interactive "sTitle: \nsPermalink for post '%s': \n")
+  (let* ((time (current-time))
+         (date (format-time-string "%Y-%m-%d" time))
+         (filename (format "%s-%s.md" date permalink))
+         (header (mblog--generate-header title)))
+    (find-file (expand-file-name filename mblog-posts-directory))
+    (insert header)
+    (newline)))
+
+;;;###autoload
+(defun mblog-find-post ()
+  "Find one of your blog posts."
   (interactive)
-  (let ((text (read-string "text: "))
-        (extra (read-string "extra text: ")))
-    (insert (format "<ruby>%s<rp>(</rp><rt>%s</rt><rp>)</rp></ruby>" text extra))))
+  (let ((files (directory-files mblog-posts-directory
+                                nil
+                                "^[^\\.].*\\.md$")))
+    (find-file (expand-file-name (ivy-read "Post: " files)
+                                 mblog-posts-directory))))
 
-(bind-key "C-c C-s r" #'my/markdown-insert-ruby-tag markdown-mode-map)
+(with-eval-after-load 'markdown-mode
+  (bind-key "C-c C-s r" #'mblog-insert-ruby-tag markdown-mode-map))
 
 (provide 'init-blog)
 
