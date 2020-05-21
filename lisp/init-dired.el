6f27191bdd81e0d26e8c41e68458f4b490a1ca21
condy <condy0919@gmail.com>
2020-05-22 05:31:21 +0800

    Update dired shell guess list

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -34,7 +34,23 @@
                             ".ccls-cache" ".clangd"
                             ".elc" ".pyc" ".o" ".swp" ".class")))
   ;; Dont prompt about killing buffer visiting delete file
-  (dired-clean-confirm-killing-deleted-buffers nil))
+  (dired-clean-confirm-killing-deleted-buffers nil)
+  (dired-guess-shell-alist-user `((,(rx "."
+                                        (or
+                                         ;; Videos
+                                         "mp4" "avi" "mkv" "flv" "ogv" "mov"
+                                         ;; Music
+                                         "wav" "mp3" "flac"
+                                         ;; Images
+                                         "jpg" "jpeg" "png" "gif" "xpm" "svg" "bmp"
+                                         ;; Docs
+                                         "pdf" "md" "djvu" "ps" "eps")
+                                        string-end)
+                                   ,(cond ((eq system-type 'gnu/linux) "xdg-open")
+                                          ((eq system-type 'darwin) "open")
+                                          ((eq system-type 'windows-nt) "start")
+                                          (t "")))))
+   )
 
 ;; Make dired colorful
 (use-package diredfl
