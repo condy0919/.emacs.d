00e642ef6143f3d0151cf1d2bba4c2544baee00f
condy <condy0919@gmail.com>
2020-05-22 20:20:58 +0800

    Ocaml lsp is stable to use

--- lisp/lang/init-ocaml.el
+++ lisp/lang/init-ocaml.el
@@ -8,27 +8,13 @@
 ;; Ocaml mode
 (use-package tuareg
   :ensure t
-  :mode ("\\.ml\\'" . tuareg-mode)
-  :bind (:map tuareg-mode-map
-         ;; consistent with lsp-mode
-         ("C-c f" . ocp-indent-region)
-         ("C-c d" . merlin-type-enclosing)))
+  :mode ("\\.ml\\'" . tuareg-mode))
 
 ;; Indentation tool for OCaml
 (use-package ocp-indent
   :ensure t
-  :hook (tuareg-mode . ocp-indent-caml-mode-setup)
-  :commands (ocp-indent-region))
-
-;; Context sensitive completion
-(use-package merlin
-  :ensure t
-  :hook (tuareg-mode . merlin-mode)
-  :commands (merlin-type-enclosing)
-  :custom
-  (merlin-error-after-save nil)
-  (merlin-command "ocamlmerlin")
-  (merlin-completion-with-doc t))
+  :hook (tuareg-mode . ocp-setup-indent)
+  :commands (ocp-indent-region ocp-indent-buffer))
 
 (provide 'init-ocaml)
 
