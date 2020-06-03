38521ebcd5e7cb09e655847c2ef5e2cbb76c795a
condy <condy0919@gmail.com>
2020-06-04 00:11:47 +0800

    Make escape more nature within minibuffer

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -205,7 +205,16 @@
 ;; Make escape more nature
 (use-package minibuffer
   :ensure nil
-  :bind ([escape] . abort-recursive-edit))
+  :bind (:map minibuffer-local-map
+         ([escape] . abort-recursive-edit)
+         :map minibuffer-local-ns-map
+         ([escape] . abort-recursive-edit)
+         :map minibuffer-local-completion-map
+         ([escape] . abort-recursive-edit)
+         :map minibuffer-local-must-match-map
+         ([escape] . abort-recursive-edit)
+         :map minibuffer-local-isearch-map
+         ([escape] . abort-recursive-edit)))
 
 ;; What day is it today?
 (use-package calendar
