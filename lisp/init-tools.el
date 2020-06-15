3ab5a8f363d8fac60df332b9a7bd9950eccca839
condy <condy0919@gmail.com>
2020-06-16 06:18:28 +0800

    Update shackle rules

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -291,32 +291,6 @@ Show the heading too, if it is currently invisible."
   (bm-in-lifo-order t)
   (bm-recenter t))
 
-;; Customize popwin behavior
-(use-package shackle
-  :ensure t
-  :hook (after-init . shackle-mode)
-  :custom
-  (shackle-default-size 0.5)
-  (shackle-default-alignment 'below)
-  (shackle-rules '((magit-status-mode    :select t :inhibit-window-quit t :same t)
-                   (magit-log-mode       :select t :inhibit-window-quit t :same t)
-                   ("*quickrun*"         :select t :inhibit-window-quit t :same t)
-                   (profiler-report-mode :select t)
-                   (apropos-mode         :select t :align t :size 0.3)
-                   (help-mode            :select t :align t :size 0.4)
-                   (comint-mode          :select t :align t :size 0.4)
-                   (grep-mode            :select t :align t)
-                   (rg-mode              :select t :align t)
-                   ("*bm-bookmarks*"           :select t   :align t)
-                   ("*Flycheck errors*"        :select t   :align t :size 10)
-                   ("*Backtrace*"              :select t   :align t :size 15)
-                   ("*Shell Command Output*"   :select nil :align t :size 0.4)
-                   ("*Async Shell Command*"    :ignore t)
-                   ("*package update results*" :select nil :align t :size 10)
-                   ("*Occur*"                  :select t   :align right)
-                   ("\\*ivy-occur .*\\*"       :regexp t :select t :align right)))
-  )
-
 ;; Grammar & Style checker
 (use-package langtool
   :ensure t
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -20,6 +20,33 @@
   (doom-modeline-unicode-fallback t)
   :hook (after-init . doom-modeline-mode))
 
+;; Customize popwin behavior
+(use-package shackle
+  :ensure t
+  :hook (after-init . shackle-mode)
+  :custom
+  (shackle-default-size 0.5)
+  (shackle-default-alignment 'below)
+  (shackle-rules '((magit-status-mode    :select t :inhibit-window-quit t :same t)
+                   (magit-log-mode       :select t :inhibit-window-quit t :same t)
+                   ("*quickrun*"         :select t :inhibit-window-quit t :same t)
+                   (profiler-report-mode :select t)
+                   (apropos-mode         :select t :align t :size 0.3)
+                   (help-mode            :select t :align t :size 0.4)
+                   (comint-mode          :select t :align t :size 0.4)
+                   (grep-mode            :select t :align t)
+                   (rg-mode              :select t :align t)
+                   ("*bm-bookmarks*"           :select t   :align t)
+                   ("*Flycheck errors*"        :select t   :align t :size 10)
+                   ("*Backtrace*"              :select t   :align t :size 15)
+                   ("*Shell Command Output*"   :select nil :align t :size 0.4)
+                   ("*Org-Babel Error Output*" :select nil :align t :size 0.3)
+                   ("*Async Shell Command*"    :ignore t)
+                   ("*package update results*" :select nil :align t :size 10)
+                   ("*Occur*"                  :select t   :align right)
+                   ("\\*ivy-occur .*\\*"       :regexp t :select t :align right)))
+  )
+
 ;; Restore windows layout
 (use-package winner-mode
   :ensure nil
