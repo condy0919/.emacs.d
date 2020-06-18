c4ba9d832682e535b06e0945595cd4fa49e8effc
condy <condy0919@gmail.com>
2020-06-18 14:32:18 +0800

    Tweak keybindings of dumb-jump

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -228,18 +228,18 @@ Such as \"Selete deleted buffer\"
 ;; Jump to definition, used as a fallback of lsp-find-definition
 (use-package dumb-jump
   :ensure t
-  :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go)
+  :bind (("M-g j" . dumb-jump-go)
+         ("M-g J" . dumb-jump-go-other-window)
          ("M-g q" . dumb-jump-quick-look)
          ("M-g b" . dumb-jump-back)
          ("M-g i" . dumb-jump-go-prompt)
-         ("M-g x" . dumb-jump-go-prefer-external)
-         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+         ("M-g o" . dumb-jump-go-prefer-external)
+         ("M-g O" . dumb-jump-go-prefer-external-other-window))
   :custom
   (dumb-jump-quiet t)
   (dumb-jump-aggressive t)
   (dumb-jump-selector 'ivy)
-  (dump-jump-prefer-searcher 'rg))
+  (dumb-jump-prefer-searcher 'rg))
 
 ;; pulse current line
 (use-package pulse
