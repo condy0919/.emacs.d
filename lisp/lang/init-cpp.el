3917265334f2ef2b570aefcbd8f2258ae946160d
condy <condy0919@gmail.com>
2020-06-27 18:27:17 +0800

    Fixed: mysterious

--- lisp/lang/init-cpp.el
+++ lisp/lang/init-cpp.el
@@ -48,7 +48,7 @@
 ;; Highlight "#if 0" as comments
 (use-package hideif
   :ensure nil
-  :hook (c-mode-common . hide-ifdef-mode)
+  :hook ((c-mode c++-mode) . hide-ifdef-mode)
   :custom
   (hide-ifdef-initially t)
   (hide-ifdef-shadow t)
