d3faee04492ee48db295a4f29ebca6c4b7713cfa
condy <condy0919@gmail.com>
2020-06-29 04:54:53 +0800

    Refine hippie-expand

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -321,7 +321,19 @@
 ;; Better abbrev expansion
 (use-package hippie-exp
   :ensure nil
-  :bind ("M-/" . hippie-expand))
+  :bind ("M-/" . hippie-expand)
+  :custom
+  (hippie-expand-try-functions-list '(try-expand-dabbrev
+                                      try-expand-dabbrev-all-buffers
+                                      try-expand-dabbrev-from-kill
+                                      try-complete-file-name-partially
+                                      try-complete-file-name
+                                      try-expand-all-abbrevs
+                                      try-expand-list
+                                      try-expand-line
+                                      try-complete-lisp-symbol-partially
+                                      try-complete-lisp-symbol))
+  )
 
 ;; Make align be a simple thing
 (use-package align
