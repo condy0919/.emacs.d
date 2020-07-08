43414e717e624e8a4030f6a7146785ca8c13d050
condy <condy0919@gmail.com>
2020-07-09 00:41:00 +0800

    Reuse go doc buffer

--- lisp/lang/init-go.el
+++ lisp/lang/init-go.el
@@ -12,7 +12,8 @@
   :config
   (with-eval-after-load 'exec-path-from-shell
     (exec-path-from-shell-copy-envs '("GOPATH" "GOPROXY")))
-  )
+  :custom
+  (godoc-reuse-buffer t))
 
 (provide 'init-go)
 
