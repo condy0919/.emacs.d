a42b591b51c206dfcee814fa7bb4b2975931cd22
Zhiwei Chen <chenzhiwei03@kuaishou.com>
2020-08-27 14:10:12 +0800

    Add qrencode interface

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -14,6 +14,19 @@
         (shell-command-on-region (region-beginning) (region-end) "ydcv")
       (shell-command (format "ydcv %s" (word-at-point))))))
 
+;;;###autoload
+(defun qrencode-on-region (start end)
+  "Call `qrencode' from START to END."
+  (interactive "r")
+  (let ((buf (get-buffer-create "*QRCode*"))
+        (inhibit-read-only t))
+    (with-current-buffer buf
+      (erase-buffer))
+    (let ((coding-system-for-read 'raw-text))
+      (shell-command-on-region start end "qrencode -o -" buf))
+    (switch-to-buffer buf)
+    (image-mode)))
+
 (provide 'init-utils)
 
 ;;; init-utils.el ends here
