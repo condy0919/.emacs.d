67c0aba1bcf1ea61a0f83c0267c3f13c4b901f50
condy <condy0919@gmail.com>
2020-09-15 17:01:17 +0800

    Remove the obsoletes since 7d30ca7f

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -18,7 +18,6 @@
   (dired-bind-vm nil)
   (dired-bind-man nil)
   (dired-bind-info nil)
-  (dired-bind-jump nil)
   (dired-auto-revert-buffer t)
   (dired-hide-details-hide-symlink-targets nil)
   (dired-listing-switches "-Afhlv"))
@@ -48,7 +47,6 @@
 (use-package dired-x
   :ensure nil
   :hook (dired-mode . dired-omit-mode)
-  :bind ([remap list-directory] . dired-jump)
   :custom
   (dired-omit-verbose nil)
   (dired-omit-files (rx (or ".DS_Store"
