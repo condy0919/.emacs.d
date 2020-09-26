f13a06c05d63c6b3aef889d3ac702b5d21b0fc1e
condy <condy0919@gmail.com>
2020-09-27 00:01:13 +0800

    Specify the output buffer name of ydcv-dwim

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -5,14 +5,16 @@
 
 ;;; Code:
 
+(defconst ydcv-buffer-name "*ydcv*")
+
 ;;;###autoload
 (defun ydcv-dwim ()
   "Call `ydcv' with active region or current symbol."
   (interactive)
   (let ((max-mini-window-height 0))
     (if (use-region-p)
-        (shell-command-on-region (region-beginning) (region-end) "ydcv")
-      (shell-command (format "ydcv %s" (word-at-point))))))
+        (shell-command-on-region (region-beginning) (region-end) "ydcv" ydcv-buffer-name)
+      (shell-command (format "ydcv %s" (word-at-point)) ydcv-buffer-name))))
 
 ;;;###autoload
 (defun qrencode-on-region (start end)
