de6a8d30e73ffec4670e272b39781cf87cb5f26a
condy <condy0919@gmail.com>
2020-09-28 12:51:46 +0800

    Enable ocp-indent/dune conditionally

--- lisp/lang/init-ocaml.el
+++ lisp/lang/init-ocaml.el
@@ -14,6 +14,7 @@
 ;; Bundled with system package `ocaml-ocp-indent'
 (use-package ocp-indent
   :ensure nil
+  :when (executable-find "ocp-indent")
   :commands (ocp-indent-region ocp-indent-buffer)
   :hook (tuareg-mode . ocp-setup-indent))
 
@@ -21,6 +22,7 @@
 ;; Bundled with system package `dune'
 (use-package dune
   :ensure nil
+  :when (executable-find "dune")
   :mode ("dune\\(?:\\.inc\\)?\\'" . dune-mode))
 
 (provide 'init-ocaml)
