7313496acc83af8846b210a7e917a52243590211
condy <condy0919@gmail.com>
2020-10-25 16:12:23 +0800

    Bring c-macro-expand back

--- lisp/lang/init-cpp.el
+++ lisp/lang/init-cpp.el
@@ -173,6 +173,13 @@
   :custom
   (ebrowse--indentation 2))
 
+;; Expand C macros
+(use-package cmacexp
+  :ensure nil
+  :custom
+  (c-macro-prompt-flag t)
+  (c-macro-shrink-window-flag t))
+
 (use-package modern-cpp-font-lock
   :ensure t
   :hook (c++-mode . modern-c++-font-lock-mode)
