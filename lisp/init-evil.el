9ea20e80c01444d088c264fc382d134b8a77c0a8
condy <condy0919@gmail.com>
2020-11-06 13:44:37 +0800

    Remove boxquote

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -171,9 +171,6 @@
     "it" 'insert-date-time
     "iu" 'counsel-unicode-char
     "iy" 'clipboard-yank
-    "ib" '(:ignore t :which-key "boxquote")
-    "ibt" 'boxquote-text
-    "iby" 'boxquote-yank
 
     ;; git
     "g" '(:ignore t :which-key "git")
