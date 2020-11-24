29dc9f2f309713e8aa24416a6624a2ce5041d700
condy <condy0919@gmail.com>
2020-11-24 20:48:17 +0800

    Refine hyperbole

--- lisp/init-hyperbole.el
+++ lisp/init-hyperbole.el
@@ -10,7 +10,14 @@
   :custom
   (hmouse-middle-flag nil)
   (inhibit-hsys-org t)
-  (inhibit-hyperbole-messaging t))
+  (inhibit-hyperbole-messaging t)
+  ;; role
+  (hyrolo-date-format "%Y-%m-%d")
+  (hyrolo-google-contacts-flag nil)
+  (hyrolo-kill-buffers-after-use t)
+  ;; window control
+  (hycontrol-invert-mode-line-flag nil)
+  (hycontrol-keep-window-flag t))
 
 (provide 'init-hyperbole)
 ;;; init-hyperbole.el ends here
