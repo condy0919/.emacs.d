893d0ad12a6ec329ac9142ecdb2f22cf03217e76
condy <condy0919@gmail.com>
2020-12-24 11:00:59 +0800

    Add ydcv shackle rule

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -42,6 +42,7 @@
                    (rg-mode              :select t :align t)
                    ("*Flycheck errors*"         :select t   :align t :size 10)
                    ("*Backtrace*"               :select t   :align t :size 15)
+                   ("*ydcv*"                    :select nil :align t :size 0.4)
                    ("*Shell Command Output*"    :select nil :align t :size 0.4)
                    ("*Async Shell Command*"     :select nil :align t :size 0.4)
                    ("*Org-Babel Error Output*"  :select nil :align t :size 0.3)
