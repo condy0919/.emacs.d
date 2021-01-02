47434df5559651f631aae5db93e1a0bbb3a48a9a
condy <condy0919@gmail.com>
2021-01-02 23:46:22 +0800

    My C++ indentation taste

--- lisp/lang/init-cpp.el
+++ lisp/lang/init-cpp.el
@@ -16,8 +16,6 @@
                     (setq comment-start "// "
                           comment-end "")))
   :config
-  (setq c-basic-offset 4)
-
   (defconst ccls-args nil)
   (defconst clangd-args '("-j=2"
                           "--background-index"
@@ -41,23 +39,220 @@
                              (awk-mode . "#+")
                              (other    . "//+\\|\\**")))
   (c-doc-comment-style `((c-mode   . gtkdoc)
-                         ,(if (>= emacs-major-version 28)
-                               '(c++-mode . doxygen)
-                             '(c++-mode . gtkdoc))))
-  (c-offsets-alist '((inline-open           . 0)
-                     (brace-list-open       . 0)
-                     (inextern-lang         . 0)
-                     (statement-case-open   . 4)
-                     (statement-cont        . (c-lineup-ternary-bodies +))
+                         (c++-mode . ,(if (>= emacs-major-version 28) 'doxygen 'gtkdoc))))
+  (c-basic-offset 4)
+  (c-label-minimum-indentation 0)
+  (c-offsets-alist '(;; a multi-line C style block comment
+                     ;;
+                     ;; /**
+                     ;;  * text
+                     ;;  */
+                     ;; int foo();
+                     (c                     . c-lineup-C-comments)
+                     ;; a multi-line string
+                     ;;
+                     ;; const char* s = "hello,\
+                     ;; world";
+                     (string                . c-lineup-dont-change)
+                     ;; brace of function
+                     ;;
+                     ;; int add1(int x) {
+                     ;;     return ++x;
+                     ;; }
+                     (defun-open            . 0)
+                     (defun-close           . 0)
+                     (defun-block-intro     . +)
+                     ;; brace of class
+                     ;;
+                     ;; class Foo {
+                     ;; public:                                 // <- access-label
+                     ;; };
+                     (class-open            . 0)
+                     (class-close           . 0)
                      (access-label          . -)
-                     (case-label            . 0)
-                     (member-init-intro     . +)
+                     ;; brace of class method
+                     ;;
+                     ;; class Foo {
+                     ;;     friend class Bar;                   // <- friend
+                     ;;     int getVar() {                      // <- inclass
+                     ;;         return 42;
+                     ;;     }
+                     ;; };
+                     (inline-open           . 0)
+                     (inline-close          . 0)
+                     (inclass               . +)
+                     (friend                . 0)
+                     ;; `noexcept' specifier indentation
+                     (func-decl-cont        . +)
+                     ;; brace of list
+                     ;;
+                     ;; int nums[] =
+                     ;; {
+                     ;;     0,
+                     ;;     1,
+                     ;;     {2},
+                     ;; };
+                     (brace-list-open       . 0)
+                     (brace-list-close      . 0)
+                     (brace-list-intro      . +)
+                     (brace-list-entry      . 0)
+                     (brace-entry-open      . 0)
+                     ;; brace of namespace
+                     ;;
+                     ;; namespace ns {
+                     ;; const int var = 42;
+                     ;; }
+                     (namespace-open        . 0)
+                     (namespace-close       . 0)
+                     (innamespace           . 0)
+                     ;; brace of statement block
+                     ;;
+                     ;; int send_mail() {
+                     ;;     std::mutex io_mtx;
+                     ;;     {
+                     ;;         std::lock_guard<std::mutex> lk(io_mtx);
+                     ;;         // ...
+                     ;;     }
+                     ;; }
+                     (block-open            . 0)
+                     (block-close           . 0)
+                     ;; topmost definition
+                     ;;
+                     ;; struct
+                     ;; foo {};
                      (topmost-intro         . 0)
-                     (inlambda              . 0) ;; better indentation for lambda
-                     (innamespace           . -) ;; no indentation after namespace
-                     (arglist-cont-nonempty . +))))
+                     (topmost-intro-cont    . c-lineup-topmost-intro-cont)
 
-;; Superb compiler explorer implementation
+                     ;; TODO ??
+                     (annotation-top-cont   . +)
+                     (annotation-var-cont   . +)
+
+                     ;; class member initialization list
+                     ;;
+                     ;; struct foo {
+                     ;;     foo(int a, int b) :
+                     ;;         a_(a),
+                     ;;         b_(b) {}
+                     ;; };
+                     (member-init-intro     . +)
+                     (member-init-cont      . c-lineup-multi-inher)
+                     ;; class inheritance
+                     ;;
+                     ;; struct Derived : public Base1,
+                     ;;                  public Base2 {
+                     ;; };
+                     (inher-intro           . +)
+                     (inher-cont            . c-lineup-multi-inher)
+                     ;; A C statement
+                     ;;
+                     ;; int main(int argc, char* argv[]) {
+                     ;;     const int var1 = 42;
+                     ;;     const int var2 = (argc > 1) ? 314   // <- a new statement starts
+                     ;;                                 : 512;  // <- statement-cont
+                     ;;     {
+                     ;;         const int var3 = 42;            // <- statement-block-intro
+                     ;;     }
+                     ;;
+                     ;;     switch (argc) {
+                     ;;     case 0:                             // <- case-label
+                     ;;         break;                          // <- statement-case-intro
+                     ;;
+                     ;;     case 1:
+                     ;;         {                               // <- statement-case-open
+                     ;;             const int tmp = 101;
+                     ;;         }
+                     ;;         break;
+                     ;;     }
+                     ;;
+                     ;;     if (argc == 1)
+                     ;;         assert(argc == 1);              // <- substatement
+                     ;;
+                     ;;     if (argc == 1)
+                     ;;     {                                   // <- substatement-open
+                     ;;         assert(argc == 1);
+                     ;;     }
+                     ;;
+                     ;;     // comments                         // <- comment-intro
+                     ;;     if (argc == 1)
+                     ;;     glabel:                             // <- substatement-label
+                     ;;         assert(argc == 1);
+                     ;;
+                     ;;  error:                                 // <- label
+                     ;;     return -1;
+                     ;; }
+                     (statement             . 0)
+                     (statement-cont        . (c-lineup-ternary-bodies +))
+                     (statement-block-intro . +)
+                     (statement-case-intro  . +)
+                     (statement-case-open   . +)
+                     (substatement          . +)
+                     (substatement-open     . 0)
+                     (substatement-label    . 0)
+                     (case-label            . 0)
+                     (label                 . 0)
+                     (do-while-closure      . 0)
+                     (else-clause           . 0)
+                     (catch-clause          . 0)
+                     (comment-intro         . c-lineup-comment)
+                     ;; funcall with arglist
+                     ;;
+                     ;; sum(1,
+                     ;;     2,
+                     ;;     3
+                     ;; );
+                     (arglist-intro         . +)
+                     (arglist-cont          . 0)
+                     (arglist-cont-nonempty . c-lineup-arglist)
+                     (arglist-close         . c-lineup-close-paren)
+                     ;; operator>> and operator<< for cin/cout
+                     ;;
+                     ;; std::cin >> a
+                     ;;          >> b;
+                     ;; std::cout << a
+                     ;;           << b;
+                     (stream-op             . c-lineup-streamop)
+                     ;; macros
+                     ;;
+                     ;; #define ALIST(G)                                \
+                     ;;     G(1)                                        \
+                     ;;     G(2)
+                     (cpp-macro             . -1000)
+                     (cpp-macro-cont        . +)
+                     ;; extern
+                     ;;
+                     ;; extern "C" {
+                     ;; void test();
+                     ;; }
+                     (extern-lang-open      . 0)
+                     (extern-lang-close     . 0)
+                     (inextern-lang         . 0)
+                     ;; lambda
+                     ;;
+                     ;; auto f = [](int a, int b) {
+                     ;;     return a + b;
+                     ;; };
+                     (inlambda              . 0)
+                     (lambda-intro-cont     . +)
+                     ;; GNU extension, a compound statement as expression
+                     ;;
+                     ;; int x = 1, y = 2;
+                     ;; int z = ({
+                     ;;     int ret;
+                     ;;     if (y > 0)
+                     ;;         ret = y;
+                     ;;     else
+                     ;;         ret = x - y;
+                     ;;     ret;
+                     ;; });
+                     (inexpr-statement      . 0)
+                     ;; c++ template args
+                     ;;
+                     ;; dummy<int,
+                     ;;       char,
+                     ;;       double>(0, 0, 0);
+                     (template-args-cont    . (c-lineup-template-args +)))))
+
+;; A compiler output viewer
 (use-package rmsbolt
   :ensure t
   :commands rmsbolt-compile
@@ -200,16 +395,6 @@
                          'c++-tempo-tags)
   )
 
-;; C++ class browser in Emacs
-(use-package ebrowse
-  :ensure nil
-  :config
-  (with-eval-after-load 'evil
-    (evil-set-initial-state 'ebrowse-tree-mode 'emacs)
-    (evil-set-initial-state 'ebrowse-member-mode 'emacs))
-  :custom
-  (ebrowse--indentation 2))
-
 (use-package modern-cpp-font-lock
   :ensure t
   :hook (c++-mode . modern-c++-font-lock-mode)
@@ -238,10 +423,10 @@
   :config
   (my/set-company-backends-for cmake-mode company-cmake))
 
+;; Extra font locks for cmake
 (use-package cmake-font-lock
   :ensure t
   :hook (cmake-mode . cmake-font-lock-activate))
-
 
 ;; Snippets for cmake
 (use-package tempo
@@ -255,8 +440,7 @@
     (tempo-use-tag-list 'cmake-tempo-tags))
 
   ;; cmake provides `PROJECT_NAME` to refer the name defined with `project`
-  ;; function. Unluckily converting a string to upper case is not an easy way in
-  ;; cmake.
+  ;; function. Sadly converting a string to upper case is hard in cmake.
   (defun proj-prefixed (arg)
     (tempo-process-and-insert-string (concat (upcase (tempo-lookup-named 'proj)) arg)))
 
@@ -285,10 +469,9 @@
   ;;
   ;; # Features
   ;;
-  ;; - Option to use libc++
   ;; - Option to enable tests
   ;; - Option to enabel benchmarks
-  ;; - Tag based version
+  ;; - Git tag based version
   (tempo-define-template "cmake-library"
                          '((P "project: " proj 'noinsert)
                            "cmake_minimum_required(VERSION 3.11)" n n
@@ -299,7 +482,6 @@
                            "  DESCRIPTION \"\"" n
                            "  HOMEPAGE_URL \"\"" n
                            ")" n n
-                           "OPTION(" (proj-prefixed "_ENABLE_LIBCXX") " \"Use the libc++ library\" OFF)" n
                            "OPTION(" (proj-prefixed "_ENABLE_TESTS") " \"Enable tests\" ON)" n
                            "OPTION(" (proj-prefixed "_ENABLE_BENCHMARKS") " \"Enable benchmarks\" OFF)" n n
                            "# Sources for the library are specified at the end" n
@@ -321,11 +503,6 @@
                            "set(THREADS_PREFER_PTHREAD_FLAG ON)" n
                            "find_package(Threads REQUIRED)" n
                            "target_link_libraries(" (s proj) " PRIVATE Threads::Threads)" n n
-                           "# Link libc++" n
-                           "if(" (proj-prefixed "_ENABLE_LIBCXX)") n
-                           "  target_compile_options(" (s proj) " PRIVATE -stdlib=libc++)" n
-                           "  target_link_libraries(" (s proj) " PRIVATE c++ c++abi)" n
-                           "endif()" n n
                            "# Benchmark" n
                            "if(" (proj-prefixed "_ENABLE_BENCHMARKS)") n
                            "  set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL \"Disable benchmark testing\" FORCE)" n
