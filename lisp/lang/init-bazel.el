20d9738b6574838a3f771850f39ce266b255ca6a
condy <condy0919@gmail.com>
2021-01-13 11:09:28 +0800

    Enable 'buildifier' conditionally

--- lisp/lang/init-bazel.el
+++ lisp/lang/init-bazel.el
@@ -10,7 +10,7 @@
   :mode (("/WORKSPACE\\'"         . bazel-workspace-mode)
          ("/WORKSPACE\\.bazel\\'" . bazel-workspace-mode))
   :custom
-  (bazel-mode-buildifier-before-save t))
+  (bazel-mode-buildifier-before-save (executable-find "buildifier")))
 
 ;; Bundled with `bazel-mode'
 (use-package bazel-build
