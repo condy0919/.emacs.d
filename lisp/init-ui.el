a7786d8c5b100796a90d03d1d1e8a54e16d4db0c
condy <condy0919@gmail.com>
2021-04-16 10:46:00 +0800

    Prefer bottom position

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -49,8 +49,8 @@
                    ("*package update results*"  :select nil :align t :size 10)
                    ("*Process List*"            :select t   :align t :size 0.3)
                    ("*Help*"                    :select t   :align t :size 0.3)
-                   ("*Occur*"                   :select t   :align right)
-                   ("\\*Embark Collect\\*.*"    :select t   :align right :regexp t)
+                   ("*Occur*"                   :select t   :align t)
+                   ("*Embark Collect*"          :select t   :align t)
                    ("\\*eldoc\\( for \\)?.*\\*" :select nil :align t :size 15 :regexp t)))
   )
 
