607f1b152497344bd1203c155024c482bff194ab
condy <condy0919@gmail.com>
2021-04-22 18:31:15 +0800

    Refactor init.el

--- init.el
+++ init.el
@@ -5,24 +5,23 @@
 ;; A big contributor to startup times is garbage collection. We up the gc
 ;; threshold to temporarily prevent it from running, and then reset it later
 ;; using a hook.
+;;
+;; It's reset by the `gcmh' module.
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
-;; Keep a ref to the actual file-name-handler
-(defvar default-file-name-handler-alist file-name-handler-alist)
+(unless (or (daemonp) noninteractive)
+  ;; Keep a ref to the actual file-name-handler
+  (let ((default-file-name-handler-alist file-name-handler-alist))
+    ;; Set the file-name-handler to nil (because regexing is cpu intensive)
+    (setq file-name-handler-alist nil)
+    ;; Reset file-name-handler-alist after initialization
+    (add-hook 'emacs-startup-hook
+              (lambda ()
+                (setq file-name-handler-alist default-file-name-handler-alist)))))
 
-;; Set the file-name-handler to nil (because regexing is cpu intensive)
-(setq file-name-handler-alist nil)
-
-;; Reset file-name-handler-alist after initialization
-(add-hook 'emacs-startup-hook
-  (lambda ()
-    (setq gc-cons-threshold 16777216
-          gc-cons-percentage 0.1
-          file-name-handler-alist default-file-name-handler-alist)))
-
-;; Increase the amount of data from the process
-;; `lsp-mode' gains
+;; Increase how much is read from processes in a single chunk (default is 4kb).
+;; `lsp-mode' gains a much
 (setq read-process-output-max (* 1024 1024))
 
 (require 'package)
