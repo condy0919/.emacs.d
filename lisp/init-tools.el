a34b3494f0ffbf75327f57e9c40191b12c67a42a
condy <condy0919@gmail.com>
2021-05-24 18:26:09 +0800

    Disable rcirc track minor mode

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -169,10 +169,7 @@
 ;; IRC client
 (use-package rcirc
   :ensure nil
-  :hook ((rcirc-mode . rcirc-track-minor-mode)
-         (rcirc-mode . rcirc-omit-mode))
-  :config
-  (add-to-list 'yank-excluded-properties 'rcirc-text)
+  :hook (rcirc-mode . rcirc-omit-mode)
   :custom
   (rcirc-default-port 7000)
   (rcirc-always-use-server-buffer-flag t)
