6f4ebe909bab36525159a64ab1a996ee5ba512f9
condy <condy0919@gmail.com>
2021-08-16 16:01:41 +0800

    Remove obsolete variables

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -47,7 +47,6 @@
     (kbd "C-c C-e") 'occur-edit-mode)
   :custom
   (evil-collection-calendar-want-org-bindings t)
-  (evil-collection-company-use-tng nil)
   (evil-collection-outline-bind-tab-p nil)
   (evil-collection-setup-minibuffer nil)
   (evil-collection-setup-debugger-keys nil))
