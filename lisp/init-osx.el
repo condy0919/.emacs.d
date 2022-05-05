dcbde8f1766ff0ac1c9fc82be46f11a193d2843e
condy <condy0919@gmail.com>
2022-05-05 14:45:37 +0800

    Properly set emoji charset in Emacs 27

--- lisp/init-osx.el
+++ lisp/init-osx.el
@@ -14,17 +14,20 @@
   ;; Make titlebar dark
   (add-to-list 'default-frame-alist '(ns-appearance . dark))
   (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+
   ;; Useful when use an external keyboard
   (defun +osx-swap-option-and-command ()
     "Swap `mac-option-modifier' and `mac-command-modifier'."
     (interactive)
     (cl-rotatef mac-option-modifier mac-command-modifier)
     (message "mac-option-modifier: %s, mac-command-modifier: %s" mac-option-modifier mac-command-modifier))
+
   ;; Emoji support
   (let ((fonts '("Apple Color Emoji")))
-    (cl-loop for font in fonts
+    (cl-loop with script = (if (>= emacs-major-version 28) 'emoji 'unicode)
+             for font in fonts
              when (member font (font-family-list))
-             return (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend)))
+             return (set-fontset-font t script (font-spec :family font) nil 'prepend)))
 
   ;; Better variable-pitch font
   (let ((fonts '("Bookerly" "Overpass" "Verdana" "Lucida Grande")))
