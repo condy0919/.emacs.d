794e1faaa02b68b7790e40b24ec5aee3bfdcdcb7
condy <condy0919@gmail.com>
2022-08-21 23:02:12 +0800

    Refine lsp semantic default behavior

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -67,21 +67,22 @@
     (lsp-enable-which-key-integration t))
   :custom
   (lsp-keymap-prefix "C-c l")
-  (lsp-enable-links nil)                 ;; no clickable links
-  (lsp-enable-folding nil)               ;; use `hideshow' instead
-  (lsp-enable-snippet nil)               ;; no snippets, it requires `yasnippet'
-  (lsp-enable-file-watchers nil)         ;; performance matters
-  (lsp-enable-text-document-color nil)   ;; as above
-  (lsp-enable-symbol-highlighting nil)   ;; as above
-  (lsp-enable-on-type-formatting nil)    ;; as above
-  (lsp-enable-indentation nil)           ;; don't change my code without my permission
-  (lsp-headerline-breadcrumb-enable nil) ;; keep headline clean
-  (lsp-modeline-code-actions-enable nil) ;; keep modeline clean
-  (lsp-modeline-diagnostics-enable nil)  ;; as above
-  (lsp-log-io nil)                       ;; debug only
-  (lsp-auto-guess-root t)                ;; Yes, I'm using projectile
-  (lsp-keep-workspace-alive nil)         ;; auto kill lsp server
-  (lsp-eldoc-enable-hover nil))          ;; disable eldoc hover
+  (lsp-enable-links nil)                    ;; no clickable links
+  (lsp-enable-folding nil)                  ;; use `hideshow' instead
+  (lsp-enable-snippet nil)                  ;; no snippets, it requires `yasnippet'
+  (lsp-enable-file-watchers nil)            ;; performance matters
+  (lsp-enable-text-document-color nil)      ;; as above
+  (lsp-enable-symbol-highlighting nil)      ;; as above
+  (lsp-enable-on-type-formatting nil)       ;; as above
+  (lsp-enable-indentation nil)              ;; don't change my code without my permission
+  (lsp-semantic-tokens-apply-modifiers nil) ;; don't override token faces
+  (lsp-headerline-breadcrumb-enable nil)    ;; keep headline clean
+  (lsp-modeline-code-actions-enable nil)    ;; keep modeline clean
+  (lsp-modeline-diagnostics-enable nil)     ;; as above
+  (lsp-log-io nil)                          ;; debug only
+  (lsp-auto-guess-root t)                   ;; Yes, I'm using projectile
+  (lsp-keep-workspace-alive nil)            ;; auto kill lsp server
+  (lsp-eldoc-enable-hover nil))             ;; disable eldoc hover
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
