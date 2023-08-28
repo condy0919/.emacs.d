243432343e7b3267094d025b196140c779ea0f96
condy <condy0919@gmail.com>
2023-08-29 01:42:46 +0800

    Dump pyright config when venv is up

--- lisp/lang/init-python.el
+++ lisp/lang/init-python.el
@@ -14,7 +14,21 @@
 ;; python -m venv ENV_DIR
 (use-package pyvenv
   :ensure t
-  :commands pyvenv-deactivate pyvenv-deactivate)
+  :commands pyvenv-deactivate pyvenv-activate
+  :config
+  (defun pyrightconfig-write ()
+    "Write a `pyrightconfig.json' file at the root of a project with
+`venvPath` and `venv`."
+    (let* ((vp (string-trim-right pyvenv-virtual-env "/"))
+           (root (file-name-directory vp))
+           (venv (file-name-base vp))
+           (out-file (expand-file-name "pyrightconfig.json" root)))
+      (with-temp-file out-file
+        (insert (json-encode (list :venvPath root
+                                   :venv venv))))
+      (message "Configured `%s` to use environment `%s`" out-file pyvenv-virtual-env)))
+
+  (add-hook 'pyvenv-post-activate-hooks #'pyrightconfig-write))
 
 (provide 'init-python)
 ;;; init-python.el ends here
