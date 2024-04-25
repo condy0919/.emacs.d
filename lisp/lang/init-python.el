7828ddbfcb607eb0f1693471bd1c4a9d8d8ca130
condy <condy0919@gmail.com>
2024-04-26 01:59:45 +0800

    Drop pyvenv
    I use conda

--- lisp/lang/init-python.el
+++ lisp/lang/init-python.el
@@ -11,24 +11,5 @@
   :custom
   (python-indent-guess-indent-offset-verbose nil))
 
-;; python -m venv ENV_DIR
-(use-package pyvenv
-  :ensure t
-  :commands pyvenv-deactivate pyvenv-activate
-  :config
-  (defun pyrightconfig-write ()
-    "Write a `pyrightconfig.json' file at the root of a project with
-`venvPath` and `venv`."
-    (let* ((vp (string-trim-right pyvenv-virtual-env "/"))
-           (root (file-name-directory vp))
-           (venv (file-name-base vp))
-           (out-file (expand-file-name "pyrightconfig.json" root)))
-      (with-temp-file out-file
-        (insert (json-encode (list :venvPath root
-                                   :venv venv))))
-      (message "Configured `%s` to use environment `%s`" out-file pyvenv-virtual-env)))
-
-  (add-hook 'pyvenv-post-activate-hooks #'pyrightconfig-write))
-
 (provide 'init-python)
 ;;; init-python.el ends here
