16b0b7f4c1480b696f7fab4bc755c2b73c7ad194
condy <condy0919@gmail.com>
2025-04-08 06:09:38 +0800

    markdown-mode: The details tag is already bound to C-c C-s F

--- lisp/init-text.el
+++ lisp/init-text.el
@@ -14,30 +14,22 @@
 ;; The markdown mode is awesome! unbeatable
 (use-package markdown-mode
   :ensure t
-  :init
-  (advice-add #'markdown--command-map-prompt :override #'ignore)
-  (advice-add #'markdown--style-map-prompt   :override #'ignore)
   :mode ("README\\(?:\\.md\\)?\\'" . gfm-mode)
   :hook (markdown-mode . visual-line-mode)
   :bind (:map markdown-mode-style-map
-         ("r" . markdown-insert-ruby-tag)
-         ("d" . markdown-insert-details))
+         ("r" . markdown-insert-ruby-tag))
   :config
   (defun markdown-insert-ruby-tag (text ruby)
     "Insert ruby tag with `TEXT' and `RUBY' quickly."
     (interactive "sText: \nsRuby: \n")
     (insert (format "<ruby>%s<rp>(</rp><rt>%s</rt><rp>)</rp></ruby>" text ruby)))
-
-  (defun markdown-insert-details (title)
-    "Insert details tag (collapsible) quickly."
-    (interactive "sTitle: ")
-    (insert (format "<details><summary>%s</summary>\n\n</details>" title)))
   :custom
   (markdown-header-scaling t)
   (markdown-enable-wiki-links t)
   (markdown-italic-underscore t)
   (markdown-asymmetric-header t)
   (markdown-gfm-uppercase-checkbox t)
+  (markdown-enable-prefix-prompts nil)
   (markdown-fontify-code-blocks-natively t))
 
 ;; ReStructuredText
