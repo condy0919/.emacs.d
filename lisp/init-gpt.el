71c39d3acbb016544b7f518e83052cfe218ed603
condy <condy0919@gmail.com>
2026-01-16 23:30:10 +0800

    Refine gptel

--- lisp/init-gpt.el
+++ lisp/init-gpt.el
@@ -7,7 +7,8 @@
 
 (use-package gptel
   :ensure t
-  :hook (gptel-mode . gptel-highlight-mode)
+  :hook ((gptel-mode . gptel-highlight-mode)
+         (gptel-post-stream . gptel-auto-scroll))
   :bind (:map gptel-mode-map
          ("C-c C-g" . gptel-abort))
   :config
@@ -15,15 +16,23 @@
                         :stream t
                         :key 'gptel-api-key-from-auth-source))
 
-  (gptel-make-preset 'gpt4coding
+  (gptel-make-preset 'thinking
+    :description "Enable chain-of-thought reasoning"
+    :request-params '(:think (:type "enabled")))
+
+  (gptel-make-preset 'coding
     :description "A preset optimized for coding tasks"
-    :backend "DeepSeek"
-    :model 'deepseek-reasoner
     :system "You are an expert coding assistant. Your role is to provide high-quality code solutions, refactorings, and explanations."
     :tools nil)
   :custom
-  (gptel-default-mode 'markdown-mode)
-  (gptel-model 'deepseek-reasoner))
+  (gptel-model 'deepseek-reasoner)
+  (gptel-track-media t)
+  (gptel-include-reasoning 'ignore)
+  (gptel-highlight-methods (if (display-graphic-p) '(fringe) '(margin)))
+  (gptel-default-mode 'org-mode)
+  (gptel-org-branching-context t)
+  (gptel-prompt-prefix-alist nil)
+  (gptel-response-prefix-alist nil))
 
 (provide 'init-gpt)
 ;;; init-gpt.el ends here
